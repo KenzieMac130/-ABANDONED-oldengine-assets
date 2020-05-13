9443af24b983273c8626296c3eb7719420b2b0ad
astrand130 <alexstrand6622@protonmail.com>
2020-05-13 04:37:08 -0400

    Shader Stuff

--- shaders/core/StandardScene_FX.glsl
+++ shaders/core/StandardScene_FX.glsl
@@ -3,36 +3,25 @@
 
 #include "../lib/standardlib.glsl"
 
-layout(binding = 0) uniform 
-AS_REFLECT_BEGIN(Model)
-AS_REFLECT_ENTRY(vec3, pos, "98, 0, 67.9")
-AS_REFLECT_ENTRY(vec2, pos2D, "100000, 53.3")
-AS_REFLECT_EXTERN_RESOURCE_FILE_ID(textureBasic, "textures/basic.ktx")
-AS_REFLECT_EXTERN_C_STRING(doot, "DOOT!!!")
-AS_REFLECT_END()
-model;
-
 #ifdef AS_STAGE_VERTEX
-
-AS_STANDARD_VERTEX_INPUTS()
-
-layout(location = 0) out vec2 outUV;
-layout(location = 1) out vec4 outColor;
+AS_STANDARD_VERTEX_ASSEMBLY_IN()
+AS_STANDARD_VERTEX_RASTER_IO(out)
+AS_STANDARD_SCENE_SHADER_INPUTS()
 
 void main(){
-	gl_Position = vec4(vPosition, 1.0);
-	outUV = vUV0;
-	outColor = vColor;
+	gl_Position = vec4(inVtxPosition, 1.0);
+	AS_STANDARD_VERTEX_RASTER_PASSTHROUGH();
 }
 #endif
 
 #ifdef AS_STAGE_FRAGMENT
-layout(location = 0) in vec2 inUV;
-layout(location = 1) in vec4 inColor;
+AS_STANDARD_SCENE_SHADER_INPUTS()
+
+AS_STANDARD_VERTEX_RASTER_IO(in)
 
 layout(location = 0) out vec4 fragColor;
 
 void main(){
-	fragColor = inColor;
+	AS_STANDARD_VERTEX_RASTER_DEBUG_RETURN(fragColor, asInstanceData.debugInfo)
 }
 #endif 
--- shaders/lib/engineInputs.glsl
+++ shaders/lib/engineInputs.glsl
@@ -2,27 +2,65 @@
 #define ENGINE_INPUTS_GLSL
 #include "engineConstants.glsl"
 
-layout(binding = AS_BINDING_GLOBAL_UBO) uniform AsGlobal{
-	vec4 customParams[AS_MAX_GLOBAL_CUSTOM_PARAMS];
-	double timeElapsed;
-	int debugMode;
-} asGlobal;
-
-layout(binding = AS_BINDING_VIEWPORT_UBO) uniform AsViewport{
-	float width;
-	float height;
-} asViewport;
-
 layout(set = AS_DESCSET_TEXTURE_POOL, binding = AS_BINDING_TEXTURE_POOL) uniform sampler2D AsBindlessTextures[AS_MAX_POOLED_TEXTURES];
 
-#define AS_STANDARD_VERTEX_INPUTS() \
-layout(location = 0) in vec3 vPosition;\
-layout(location = 1) in vec3 vNormal;\
-layout(location = 2) in vec3 vTangent;\
-layout(location = 3) in vec2 vUV0;\
-layout(location = 4) in vec2 vUV1;\
-layout(location = 5) in vec4 vColor;\
-layout(location = 6) in uvec4 vBoneIdxs;\
-layout(location = 7) in vec4 vBoneWeights;
+#define AS_STANDARD_VERTEX_RASTER_IO(IO) \
+layout(location = 0) IO vec3 IO##VtxPosition;\
+layout(location = 1) IO vec3 IO##VtxNormal;\
+layout(location = 2) IO vec4 IO##VtxTangent;\
+layout(location = 3) IO vec2 IO##VtxUV0;\
+layout(location = 4) IO vec2 IO##VtxUV1;\
+layout(location = 5) IO vec4 IO##VtxColor;
+
+#define AS_STANDARD_VERTEX_RASTER_PASSTHROUGH()\
+outVtxPosition = inVtxPosition;\
+outVtxNormal = inVtxNormal;\
+outVtxTangent = inVtxTangent;\
+outVtxUV0 = inVtxUV0;\
+outVtxUV1 = inVtxUV1;\
+outVtxColor = inVtxColor;
+
+#define AS_STANDARD_VERTEX_RASTER_DEBUG_RETURN(RETURN, DBG)\
+if(DBG == 0)\
+	RETURN = vec4(inVtxPosition, 1.0);\
+else if(DBG == 1)\
+	RETURN = vec4(inVtxNormal.xyz * 0.5 + vec3(0.5), 1.0);\
+else if(DBG == 2)\
+	RETURN = vec4(inVtxTangent.xyz, 1.0);\
+else if(DBG == 3)\
+	RETURN = vec4(inVtxTangent.www, 1.0);\
+else if(DBG == 4)\
+	RETURN = vec4(inVtxUV0, 0.0, 1.0);\
+else if(DBG == 5)\
+	RETURN = vec4(inVtxUV1, 0.0, 1.0);\
+else if(DBG == 6)\
+	RETURN = vec4(inVtxColor.aaa, 1.0);\
+else \
+	RETURN =  vec4(inVtxColor.rgb, 1.0);
+
+#define AS_STANDARD_VERTEX_ASSEMBLY_IN() \
+AS_STANDARD_VERTEX_RASTER_IO(in)\
+layout(location = 6) in uvec4 inVtxBoneIdxs;\
+layout(location = 7) in vec4 inVtxBoneWeights;
+
+
+#define AS_STANDARD_SCENE_SHADER_INPUTS() \
+layout(push_constant) uniform AsInstanceData {\
+	int materialIdx;\
+	int transformOffsetCurrent;\
+	int transformOffsetLast;\
+	int debugInfo;\
+} asInstanceData;\
+\
+layout(binding = AS_BINDING_GLOBAL_UBO) uniform AsGlobal{\
+	vec4 customParams[AS_MAX_GLOBAL_CUSTOM_PARAMS];\
+	double timeElapsed;\
+	int debugMode;\
+} asGlobal;\
+\
+layout(binding = AS_BINDING_VIEWPORT_UBO) uniform AsViewport{\
+	float width;\
+	float height;\
+} asViewport;
 
 #endif 
